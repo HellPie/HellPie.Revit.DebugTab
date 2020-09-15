589b8debe5b47915986c20473791678ca47cee51
Diego Rossi <git@hellpie.dev>
2020-09-15 17:37:36 +0200

    Store ribbon control to make code less repetitive

--- HellPie.Revit.DebugTab/App.cs
+++ HellPie.Revit.DebugTab/App.cs
@@ -1,15 +1,18 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Autodesk.Revit.UI;
+using UIFramework;
 
 namespace HellPie.Revit.DebugTab {
     [SuppressMessage("ReSharper", "UnusedType.Global")]
     internal class App : IExternalApplication {
         /// <inheritdoc />
         public Result OnStartup(UIControlledApplication application) {
-            UIFramework.RevitRibbonControl.RibbonControl.Tabs.Add(UIFramework.RevitRibbonControl.RibbonControl.DebugTab.Clone());
-            UIFramework.RevitRibbonControl.RibbonControl.Tabs.Last().IsVisible = true;
-            UIFramework.RevitRibbonControl.RibbonControl.Tabs.Last().Id = "Debug_Shadowed";
+            RevitRibbonControl ribbonControl = RevitRibbonControl.RibbonControl;
+
+            ribbonControl.Tabs.Add(ribbonControl.DebugTab.Clone());
+            ribbonControl.Tabs.Last().IsVisible = true;
+            ribbonControl.Tabs.Last().Id = "Debug_Shadowed";
 
             return Result.Succeeded;
         }
