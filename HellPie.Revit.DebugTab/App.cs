70a251504204b6af1aa14268f99e7ca108541f00
Diego Rossi <git@hellpie.dev>
2020-09-15 15:34:14 +0200

    Add code that enables Debug tools in Revit

--- HellPie.Revit.DebugTab/App.cs
+++ HellPie.Revit.DebugTab/App.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Autodesk.Revit.UI;
 
 namespace HellPie.Revit.DebugTab {
@@ -6,6 +7,10 @@ namespace HellPie.Revit.DebugTab {
     internal class App : IExternalApplication {
         /// <inheritdoc />
         public Result OnStartup(UIControlledApplication application) {
+            UIFramework.RevitRibbonControl.RibbonControl.Tabs.Add(UIFramework.RevitRibbonControl.RibbonControl.DebugTab.Clone());
+            UIFramework.RevitRibbonControl.RibbonControl.Tabs.Last().IsVisible = true;
+            UIFramework.RevitRibbonControl.RibbonControl.Tabs.Last().Id = "Debug_Shadowed";
+
             return Result.Succeeded;
         }
 
